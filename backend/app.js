9153a5dde74949a7054eb0e9aba7fa84c2556f9c
Fenil Savani <fenilsavani322@gmail.com>
2022-07-14 10:54:12 +0530

    fenil app.js update

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const app=express();
 
-app.use(express.json)
+// app.use(express.json)
 
 //Route Imports
 const product=require('./routes/productRoute')
-app.use("api/v1",product)
+
+app.use("api/v1",product);
+
+// app.get('',(req,res)=>{
+// res.send("helllo")
+// })
 
 module.exports=app
